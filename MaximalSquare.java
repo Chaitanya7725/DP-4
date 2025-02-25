9f5c0c182e4eb4f00b439a1ecc91d96604a81267
Chaitanya7725 <chaitanya7725@gmail.com>
2025-02-25 01:20:18 -0500

    Adding Maximal square

---
+++ MaximalSquare.java
@@ -0,0 +1,31 @@
+// TC: O(m * n) traversing all the elements
+// SC: O(m * n) for dp array
+
+// In this approach, the current elements value depends on the top, 
+// left and diagonally top left value. and min of 3 + 1 is set. 
+public class MaximalSquare {
+	public static void main(String[] args) {
+		System.out.println(maximalSquare(new String[][] { { "1", "0", "1", "0", "0" }, { "1", "0", "1", "1", "1" },
+				{ "1", "1", "1", "1", "1" }, { "1", "0", "0", "1", "0" } })); // 4
+		System.out.println(maximalSquare(new String[][] { { "0", "1" }, { "1", "0" } })); // 1
+	}
+
+	public static int maximalSquare(String[][] matrix) {
+		if (matrix == null || matrix.length == 0)
+			return 0;
+		int m = matrix.length;
+		int n = matrix[0].length;
+		int max = 0;
+		int[][] dp = new int[m + 1][n + 1];
+		for (int i = 1; i < m + 1; i++) {
+			for (int j = 1; j < n + 1; j++) {
+				if (matrix[i - 1][j - 1].equals("1")) {
+					dp[i][j] = Math.min(dp[i][j - 1], Math.min(dp[i - 1][j], dp[i - 1][j - 1])) + 1;
+					max = Math.max(max, dp[i][j]);
+				}
+			}
+		}
+		return max * max;
+	}
+
+}
